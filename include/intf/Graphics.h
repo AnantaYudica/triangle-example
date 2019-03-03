07a90467cc1761f38c7e2dff3652f28eb2eada0f
Ananta Yudica <nta040689@gmail.com>
2019-03-03 22:30:27 +0700

    Added interface class Graphics

---
+++ include/intf/Graphics.h
@@ -0,0 +1,22 @@
+#ifndef INTF_GRAPHICS_H_
+#define INTF_GRAPHICS_H_
+
+#include "Vertex.h"
+
+
+namespace intf
+{
+
+class Graphics
+{
+public:
+    Graphics() = default;
+    virtual ~Graphics() = default;
+public:
+    virtual bool InitDraw(Vertex * vertices, int size) = 0;
+    virtual bool Draw() = 0;
+};
+
+} //!intf
+
+#endif //!INTF_GRAPHICS_H_
