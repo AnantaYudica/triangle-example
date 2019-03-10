629edd287a60970b994cd108632d29e9a525da0e
Ananta Yudica <nta040689@gmail.com>
2019-03-11 05:53:13 +0700

    Added definition categories of Shader

---
+++ include/defn/categ/Shader.h
@@ -0,0 +1,20 @@
+#ifndef DEFN_SHADER_H_
+#define DEFN_SHADER_H_
+
+namespace defn
+{
+namespace categ
+{
+
+enum class Shader
+{
+    vertex,
+    pixel,
+    fragment
+};
+
+} //!categ
+
+} //!defn
+
+#endif //!DEFN_SHADER_H_
