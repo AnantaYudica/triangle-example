35a319412fdc2aec31edae33ae0ec78a1156bef2
Ananta Yudica <nta040689@gmail.com>
2019-03-20 04:02:22 +0700

    Added enum for definition graphics

---
+++ include/defn/Graphics.h
@@ -0,0 +1,16 @@
+#ifndef DEFN_GRAPHICS_H_
+#define DEFN_GRAPHICS_H_
+
+namespace defn
+{
+enum class Graphics
+{
+    undefined = 0,
+    direct3d = 1,
+    wgl = 2,
+    egl = 2
+};
+
+} //!defn
+
+#endif //!DEFN_GRAPHICS_H_
