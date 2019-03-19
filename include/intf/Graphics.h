2cf0abc20729e0f1adb25ed50b0442a21e1f109c
Ananta Yudica <nta040689@gmail.com>
2019-03-20 03:39:08 +0700

    Updated class abstract Graphics

--- include/intf/Graphics.h
+++ include/intf/Graphics.h
@@ -1,18 +1,42 @@
 #ifndef INTF_GRAPHICS_H_
 #define INTF_GRAPHICS_H_
 
+#include "graphics/Shader.h"
+#include "graphics/Vertex.h"
+#include "../defn/categ/Shader.h"
+#include "../defn/Graphics.h"
+
+
 namespace intf
 {
 
 class Graphics
 {
 public:
+    typedef defn::categ::Shader ShaderCategoryType;
+    typedef defn::Graphics CategoryType;
+public:
+    typedef intf::graphics::Shader ShaderInterfaceType;
+    typedef intf::graphics::Vertex VertexInterfaceType;
+    typedef typename ShaderInterfaceType::PointerType
+        ShaderInterfacePointerType;
+    typedef typename VertexInterfaceType::PointerType
+        VertexInterfacePointerType;
+public:
     Graphics() = default;
 public:
     virtual ~Graphics() = default;
 public:
-    virtual bool Draw() = 0;
+    virtual CategoryType Category() = 0;
+public:
+    virtual bool BeginDraw() = 0;
+    virtual bool EndDraw() = 0;
+public:
     virtual void Cleanup() = 0;
+public:
+    virtual ShaderInterfacePointerType 
+        MakeShader(const ShaderCategoryType & categ) = 0;
+    virtual VertexInterfacePointerType MakeVertex() = 0;
 };
 
 } //!intf
